7b732bca454cb086e57adef1bda776d859ce72fa
fiveseven-lambda <fiveseven.lambda@gmail.com>
2022-04-11 19:10:36 +0900

    change result alignment

--- src/result.tsx
+++ src/result.tsx
@@ -20,33 +20,34 @@ export const Result = ({input, results, pValue: [pValue, setPValue], pkg, widthS
   let [showPrecise, setShowPrecise] = React.useState(false);
   if(results.statistic !== null){
     elements.push(<div key='latest-result'>
-      <button
-      onClick={
-        _ => {
-          let item: HistoryItem = {
-              date: new Date(),
-              label: [input.label[0], input.label[1]],
-              test: input.test,
-              size: input.data.length,
-              statistic: results.statistic,
-              pvalue: null,
-          };
-          pkg.fn[input.test](input.data.length, results.statistic).then(pvalue_str => {
-            $('#progress').text('');
-            const pvalue = JSON.parse(pvalue_str) as PValue;
-            setPValue(pvalue);
-            item.pvalue = pvalue.approx;
-            setHistory([...history, item]);
-          })
-        }
-      }>compute <KaTeX text='p'/>-value</button>
+      <div>
+        <button
+        onClick={
+          _ => {
+            let item: HistoryItem = {
+                date: new Date(),
+                label: [input.label[0], input.label[1]],
+                test: input.test,
+                size: input.data.length,
+                statistic: results.statistic,
+                pvalue: null,
+            };
+            pkg.fn[input.test](input.data.length, results.statistic).then(pvalue_str => {
+              $('#progress').text('');
+              const pvalue = JSON.parse(pvalue_str) as PValue;
+              setPValue(pvalue);
+              item.pvalue = pvalue.approx;
+              setHistory([...history, item]);
+            })
+          }
+        }>compute <KaTeX text='p'/>-value</button> <span id='progress'/>
+      </div>
       <div>
         <input
           type='checkbox'
           onChange={ event => setShowPrecise(event.target.checked) }
         /><label>show precise</label>
       </div>
-      <span id='progress'/>
       <p>statistic: <KaTeX text={getStatisticName(input.test)}/> = {(showPrecise ? `${results.statistic} / ${input.data.length} =` : '')} <KaTeX text={num2tex(results.statistic / input.data.length)}/></p>
     </div>);
   }
